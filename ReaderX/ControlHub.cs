fdca218c923e78efc7888d55c96d19351ba71d19
morning-firebird <victorogniewicz@gmail.com>
2023-09-23 22:51:32 +0300

    final version with "WinForm" as the  main class and "ControlHub" as an empty mediator

--- ReaderX/ControlHub.cs
+++ ReaderX/ControlHub.cs
@@ -2,21 +2,6 @@
 {
     internal static class ControlHub
     {
-        private static MainForm _gui = new();
-        internal static MainForm GUI { get => _gui; }
-
-        internal static void InitializateProgram ()
-        {
-            InitializeEvents();
-        }
-
-        private static void InitializeEvents()
-        {
-            //GUI.
-        }
-
-
-
         internal static List<string> LoadImageList() => ImageLoader.ListImagesInFolder();
 
         internal static Image LoadSelectedImage(string fileName) => ImageLoader.LoadImage(fileName);
---
+++ ReaderX/ImageFileList.cs
@@ -0,0 +1,55 @@
+﻿namespace ReaderX
+{
+    internal class ImageFileList
+    {
+
+        internal List<string> Files 
+        { get => _imageNames; private set => _imageNames = value; }
+
+        internal string Folder 
+        { get => _folderPath; private set => _folderPath = value; }
+
+        internal int SelectedIndex
+        {
+            get => _selectedIndex;
+            set
+            {
+                int lastIndex = _imageNames.Count - 1;
+
+                if (lastIndex <= 0)
+                {
+                    value = 0;
+                }
+                else
+                {
+                    //removes excessive overflow
+                    value %= lastIndex;
+
+                    if (value < 0)
+                        value += lastIndex;
+                }
+
+                _selectedIndex = value;
+                SelectedImage = ImageLoader.LoadImage(Folder + SelectedFile);
+            }
+        }
+
+        internal string SelectedFile { get => _imageNames[_selectedIndex]; }
+
+        internal Image SelectedImage 
+        { get => _selectedImage; private set => _selectedImage = value; }
+
+        private string _folderPath;
+        private List<string> _imageNames;
+        private int _selectedIndex;
+        private Image _selectedImage;
+
+        public ImageFileList(List<string> fileNameList, string folderPath = "")
+        {
+            _imageNames = fileNameList;
+            _folderPath = folderPath;
+            _selectedIndex = 0;
+            _selectedImage = ImageLoader.LoadImage(Folder+SelectedFile);
+        }
+    }
+}
